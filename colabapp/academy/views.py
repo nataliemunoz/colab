7e37af67779ddbd1a7243761bbcb0c38c8c66b87
natalie munoz <nataliemunozh@gmail.com>
2023-07-11 01:22:24 +0000

    course.html added

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def v_index(request):
     context = {}
     return render(request, 'index.html', context) #nelaza la vista con el html
+
+def v_course(request):
+    context = {}
+    return render(request, 'course.html', context)  
+
